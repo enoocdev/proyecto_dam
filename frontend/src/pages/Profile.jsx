ce937a23686d00da1fbb148815a0522a50719e79
enooc <enooc@ENOOC>
2025-12-09 20:53:58 +0100

    profile start

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react"
+import { jwtDecode } from "jwt-decode";
+import { ACCESS_TOKEN } from "../constants";
+import api from "../api"
 
 
 function Profile(){
+    const [user,setUser] = useState()
+
+    useEffect(()=>{
+        const loadingProfileData = async () => {
+            const  token = localStorage.getItem(ACCESS_TOKEN)
+            const { user_id } = jwtDecode(token)
+            
+            const {data} = await api.get(`/users/${user_id}/`)
+
+            console.log(data)
+            
+
+            setUser(
+                JSON.stringify(data)
+            )
+        } 
+
+        loadingProfileData()
+    },[])
+
     return(
-        <>
-            profile...
-        </>
+        <div>
+            <p>{user}</p>
+        </div>
     )
 }
 
